162a9faf63beb0b4e8b8df4f3ad418d917d3882d
journy002 <journy002@naver.com>
2022-03-13 20:54:41 +0900

    update

--- src/App.js
+++ src/App.js
@@ -2,32 +2,84 @@ import logo from "./logo.svg";
 import "./App.css";
 import InputSample from "./components/inputSample";
 import InputSamples from "./components/inputSamples";
-import { useState } from "react";
+import { useState, useRef } from "react";
 import UserList from "./components/userList";
+import CreateUsers from "./components/createUsers";
 
 function App() {
+  const [inputs, setInputs] = useState({
+    name: "",
+    email: "",
+  });
+
+  const { name, email } = inputs;
+
   const [users, setUsers] = useState([
     {
       id: 1,
       name: "oms",
       email: "oms@oms@.com",
+      active: true,
     },
     {
       id: 2,
       name: "oms2",
       email: "oms2@oms@.com",
+      active: false,
     },
     {
       id: 3,
       name: "oms3",
       email: "oms3@om3s@.com",
+      active: false,
     },
   ]);
 
+  const nextId = useRef(4);
+
+  const onChange = (e) => {
+    let { name, value } = e.target;
+
+    setInputs({
+      ...inputs,
+      [name]: value,
+    });
+  };
+
+  const onCreate = () => {
+    const user = {
+      id: nextId.current,
+      name,
+      email,
+    };
+
+    setUsers([...users, user]);
+    setInputs({ name: "", email: "" });
+
+    nextId.current += 1;
+  };
+
+  const onRemove = (id) => {
+    setUsers(users.filter((user) => user.id !== id));
+  };
+
+  const onToggle = (id) => {
+    setUsers(
+      users.map((user) =>
+        user.id === id ? { ...user, active: !user.active } : user
+      )
+    );
+  };
+
   return (
     <div>
-      <InputSamples />
-      <UserList users={users} />
+      <CreateUsers
+        name={name}
+        email={email}
+        onChange={onChange}
+        onCreate={onCreate}
+      />
+      <UserList users={users} onRemove={onRemove} onToggle={onToggle} />
     </div>
   );
 }
--- src/components/userList.jsx
+++ src/components/userList.jsx
@@ -1,20 +1,34 @@
 import React from "react";
 
-function User({ user }) {
+function User({ user, onRemove, onToggle }) {
   const { name, email } = user;
   return (
     <div>
-      <strong>{name}</strong>
+      <strong
+        style={{
+          cursor: "pointer",
+          color: user.active ? "green" : "black",
+        }}
+        onClick={() => onToggle(user.id)}
+      >
+        {name}
+      </strong>
       <span>{email}</span>
+      <button onClick={() => onRemove(user.id)}>Del</button>
     </div>
   );
 }
 
-function UserList({ users }) {
+function UserList({ users, onRemove, onToggle }) {
   return (
     <div>
       {users.map((user) => (
-        <User key={user.id} user={user} />
+        <User
+          key={user.id}
+          user={user}
+          onRemove={onRemove}
+          onToggle={onToggle}
+        />
       ))}
     </div>
   );
