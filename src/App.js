c870ebe58412b86628902eca381e4f2410fdd28d
journy002 <journy002@naver.com>
2022-03-15 20:55:42 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from "./logo.svg";
+import React, { Component } from "react";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Main from "./components/main";
--- src/components/commu.jsx
+++ src/components/commu.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 function Commu() {
   return (
@@ -8,7 +8,7 @@ function Commu() {
         <Link to="/">메인</Link>
       </ul>
       <p>이곳은 함께 소통하는 공간입니다.</p>
-      <p>댓글을 통해 사람들과 자유로운 대화를 시작하세요!</p>
+      <p>대화방을 통해 사람들과 자유로운 대화를 시작하세요!</p>
     </div>
   );
 }
